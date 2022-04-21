9fa9bdaee36a2f0d90fe2591274f920f45f89ac3
vixfwis <vixfwis@gmail.com>
2022-04-21 16:21:05 +0300

    setup.py metadata

--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='simplelogin-api',
     version='0.1.0',
+    author="vixfwis",
+    description="SimpleLogin API wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vixfwis/simplelogin-api",
     packages=find_packages(),
     install_requires=[
         'attrs==21.4.0',
